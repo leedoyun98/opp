499349ec3639a619c8ec60788ec7a2e5a6a4f4c2
leedoyun98 <74306606+leedoyun98@users.noreply.github.com>
2020-11-17 12:40:46 +0900

    Add files via upload

--- day04/Calculator.java
+++ day04/Calculator.java
@@ -1,5 +1,33 @@
 package day04;
 
 public class Calculator {
-
+	int number1;
+	int number2;
+	String operator;
+	
+	public int getNumber1() {
+		return number1;
+	}
+	public void setNumber1(int number1) {
+		this.number1 = number1;
+	}
+	public int getNumber2() {
+		return number2;
+	}
+	public void setNumber2(int number2) {
+		this.number2 = number2;
+	}
+	public String getOperator() {
+		return operator;
+	}
+	public void setOperator(String operator) {
+		this.operator = operator;
+	}
+	
+	
+	
+	
+	
+	
+	
 }
--- day04/Main.java
+++ day04/Main.java
@@ -1,5 +1,71 @@
 package day04;
 
-public class Main {
+import java.util.Scanner;
 
-}
+public class Main {
+	public static void main(String[] args) {
+	
+		Scanner scanner = new Scanner(System.in);
+		Calculator cal = new Calculator();
+		
+
+		System.out.println("간단한 계산기");
+		System.out.println("숫자 1 입력");
+		int nu1 = scanner.nextInt();
+		cal.setNumber1(nu1);
+		System.out.println("+, -, * ,/ 골라주세요");
+		scanner.nextLine();
+		String n2 = scanner.nextLine();
+		cal.setOperator(n2);
+		System.out.println("숫자 2 입력");
+		int nu2 = scanner.nextInt();
+		cal.setNumber2(nu2);
+	
+	       if(cal.getOperator().equals("+") ) {
+		System.out.println(cal.getNumber1()+"+"+cal.getNumber2()+"= "+add(cal.getNumber1(),cal.getNumber2()));
+		main(args);
+		}else if(cal.getOperator().equals("-")) {
+			System.out.println(cal.getNumber1()+"-"+cal.getNumber2()+"= "+s(cal.getNumber1(),cal.getNumber2()));
+			main(args);
+		}else if(cal.getOperator().equals("*")) {
+			System.out.println(cal.getNumber1()+"*"+cal.getNumber2()+"= "+c(cal.getNumber1(),cal.getNumber2()));
+			main(args);
+		}
+			else if(cal.getOperator().equals("/")) {
+				System.out.println(cal.getNumber1()+"/"+cal.getNumber2()+"= "+d(cal.getNumber1(),cal.getNumber2()));
+				System.out.println("나머지: "+f(cal.getNumber1(),cal.getNumber2()));
+				main(args);
+						
+			}
+			
+			
+		
+		scanner.close();
+	}
+	
+	
+		static int  add(int a, int b) {	
+				int r = a+b;
+			return r;
+	
+		}
+		static int s(int a, int b) {
+			int r = a-b;
+			return r;
+		}
+		static int c(int a, int b) {
+			int r = a*b;
+			return r;
+		}
+		static int d(int a, int b) {
+			int r = a/b;
+			return r;
+		}
+		static int f(int a, int b) {
+			int r = a%b;
+			return r;
+		}
+	
+	}
+
+
